156ed7ff7868f796793a6355050d9cf1cabb7eac
andres.pena1 <andres.penaortega06@gmail.com>
2022-02-04 17:53:57 +0100

    gallina que corre

--- Player.cs
+++ Player.cs
@@ -6,15 +6,18 @@ using UnityEngine.AI;
 public class Player : MonoBehaviour {
 
     NavMeshAgent agente;
-
+    private Animator animator;
+    private Vector3 movimiento;
     void Start()
     {
         //Capturamos el componente agente del jugador
         agente = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
+        movimiento = agente.velocity;
         if (Input.GetMouseButtonDown(0))
         {
             Ray rayo = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -25,5 +28,18 @@ public class Player : MonoBehaviour {
                 agente.SetDestination(hit.point);
             }
         }
+        if (movimiento != Vector3.zero)
+
+        {
+            animator.SetBool("Run", true);
+
+        }
+        else
+        {
+
+            animator.SetBool("Run", false);
+
+        }
+
     }
 }
